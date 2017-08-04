62e025d0fac1c1ab56dc9953ec582c4e29b35ee8
arthaszhf <zhf888000@163.com>
2017-08-04 16:10:47 +0800

    HTML

---
+++ czmsy/src/main/java/com/arthas/czmsy/repository/ResourecRepository.java
@@ -0,0 +1,9 @@
+package com.arthas.czmsy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.arthas.czmsy.entity.Resource;
+
+public interface ResourecRepository extends JpaRepository<Resource, Integer> {
+
+}
